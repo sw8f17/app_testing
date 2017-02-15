e52dbc67ffbc4b235cce196af402a87e98c1c92c
Jesper Jensen <delusionalLogic@gmail.com>
2017-02-15 12:26:02 +0100

    Test script "improvements"

--- audioshift.py
+++ audioshift.py
@@ -6,6 +6,7 @@ from itertools import combinations
 from glob import glob
 import os.path
 from matplotlib import pyplot as plt
+import sounddevice as sd
 
 
 def _cross_correlation_using_fft(x, y):
@@ -13,7 +14,7 @@ def _cross_correlation_using_fft(x, y):
     Make cross correlation between two signals using fft
 
     :param x: Sample 1
-    :param y: sample 2
+    :param y: Sample 2
     :returns: Cross correlation of sample 1 and 2
     """
     f1 = fft(x)
@@ -27,7 +28,7 @@ def _compute_shift(x, y):
     Compute the time shift of signal y in regards to signal x
 
     :param x: Sample 1
-    :param y: sample 2
+    :param y: Sample 2
     :returns: < 0 means that y starts 'shift' time steps before x
         > 0 means that y starts 'shift' time steps after x
     """
@@ -38,9 +39,49 @@ def _compute_shift(x, y):
     shift = zero_index - np.argmax(np.abs(c))
     return shift
 
+@begin.subcommand
+def offline():
+    print("Offline analysis")
+    duration = 5 # seconds
+    fs = 48000
+    sd.default.samplerate = fs
+    audio = sd.rec(duration * fs, channels=2, dtype="float64")
+    sd.wait()
 
-@begin.start(auto_convert=True)
-def main(in_file: 'Wavfile(s) to test. Glob syntax can be used'):
+    print("  [{} channels, {:.2f} sec. {} Hz]".format(audio.shape[1], audio.shape[0] / fs, fs))
+    for pair in combinations(range(audio.shape[1]), 2):
+        print("  Ch. {} -> {}:".format(*pair), end='')
+        shift = _compute_shift(audio[:, pair[1]], audio[:, pair[0]])
+        print(" {:>5} sample(s) [{:>7,.3f} ms]".format(shift, shift / (fs / 1000)))
+
+    sd.play(audio)
+    sd.wait()
+
+def callback(indata, outdata, frames, time, status):
+    global buffc, buff
+    if status:
+        print(status, flush=True)
+    outdata[:] = indata
+    fs = 48000
+
+    audio = indata
+    print("  [{} channels, {:.2f} sec. {} Hz]".format(audio.shape[1], audio.shape[0] / fs, fs))
+    for pair in combinations(range(audio.shape[1]), 2):
+        print("  Ch. {} -> {}:".format(*pair), end='')
+        shift = _compute_shift(audio[:, pair[1]], audio[:, pair[0]])
+        print(" {:>5} sample(s) [{:>7,.3f} ms]".format(shift, shift / (fs / 1000)))
+
+@begin.subcommand
+def online():
+    print("Online analysis")
+    duration = 10 # seconds
+    fs = 48000
+    sd.default.samplerate = fs
+    with sd.Stream(channels=2, blocksize=1*fs, dtype="float32", callback=callback):
+        sd.sleep(duration*10000000)
+
+@begin.subcommand
+def file(in_file: 'Wavfile(s) to test. Glob syntax can be used'):
     for file in glob(in_file):
         print("\nFile:", os.path.basename(file), end='')
         rate = 0
@@ -55,3 +96,7 @@ def main(in_file: 'Wavfile(s) to test. Glob syntax can be used'):
             print("  Ch. {} -> {}:".format(*pair), end='')
             shift = _compute_shift(audio[:, pair[1]], audio[:, pair[0]])
             print(" {:>5} sample(s) [{:>7,.3f} ms]".format(shift, shift / (rate / 1000)))
+
+@begin.start(auto_convert=True)
+def main():
+    pass
